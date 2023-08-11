a60d56a80be59df76cfd043dee77e55be71b2541
Jesus Marenco <jesusmarencodev@gmail.com>
2023-08-11 15:57:10 -0500

    config @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 import { StoreModule } from '@ngrx/store';
 
@@ -8,13 +9,18 @@ import { TodoModule } from './todos/todo.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
     TodoModule,
-    StoreModule.forRoot({ text: todoReducer }),
+    StoreModule.forRoot({ todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,7 @@ import { createReducer, on } from '@ngrx/store';
 import { create } from './toto.actions';
 import { Todo } from './models/todo.model';
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [new Todo('Example')];
 
 export const todoReducer = createReducer(
   initialState,
